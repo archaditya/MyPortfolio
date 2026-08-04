1bb083c34f5d8f904b5d586eff9ae33e808c0850
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2026-08-04 11:42:25 +0530

    Update link in Hero component

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -188,7 +188,7 @@ export default function Hero() {
               </svg>
             </a>
             <a
-              href="https://drive.google.com/file/d/1-rUFHqAQ-yGJOEtWBNyJBjowoeGHlgR3/view"
+              href="https://drive.google.com/file/d/1ocDAJkHV-2fs_eAvr8YN1nvOTHXQrlBA/view?usp=drivesdk"
               className="inline-flex items-center gap-2 px-6 py-3 border border-white/[0.1] hover:border-white/[0.2] text-white/70 hover:text-white text-sm font-medium rounded-lg transition-all duration-200 group"
             >
               <svg
